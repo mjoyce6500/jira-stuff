7036d178c649c583d1ea8ef9edb932bf9b78f428
mjoyce4 <mjoyce4@bloomberg.net>
2018-08-30 13:46:25 -0400

    testing API call to public JIRA

---
+++ files/jiraAPI.ps1
@@ -0,0 +1,22 @@
+﻿$url = "https://jira.atlassian.com/rest/api/2/search?jql=project%20%3D%20JRASERVER%20AND%20resolution%20%3D%20Unresolved%20ORDER%20BY%20priority%20DESC%2C%20updated%20DESC"
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+
+# $pass = Get-Credential -UserName $user -Message "input password"
+# $pair = "${user}:${pass}"
+# $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+$basicAuthValue = "Basic $base64"
+$headers = @{ Authorization = $basicAuthValue }
+
+  try {
+    Invoke-RestMethod $url -Headers $headers
+        -Body (ConvertTo-Json $data) 
+        -ContentType "application/json" 
+        -Headers $DefaultHttpHeaders 
+        -Method Post
+    }
+    catch {
+        $streamReader = [System.IO.StreamReader]::new($_.Exception.Response.GetResponseStream())
+        $ErrResp = $streamReader.ReadToEnd() | ConvertFrom-Json
+        $streamReader.Close()
+    }
+        $ErrResp
